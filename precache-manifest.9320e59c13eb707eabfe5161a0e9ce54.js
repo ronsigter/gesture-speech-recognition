186c39a308da7f037f5d4c3481983bb7aba9deac
ronsigter <ronchester.mendoza@gmail.com>
2020-01-05 11:58:24 +0800

    Updates

---
+++ precache-manifest.9320e59c13eb707eabfe5161a0e9ce54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4909da443ebb33f70a9ca88f8a599133",
+    "url": "/gesture-speech-recognition/index.html"
+  },
+  {
+    "revision": "cab906789424caaa69d3",
+    "url": "/gesture-speech-recognition/static/js/2.f4720c53.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/gesture-speech-recognition/static/js/2.f4720c53.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d6be0fc31fc47aa4913f",
+    "url": "/gesture-speech-recognition/static/js/main.286c2609.chunk.js"
+  },
+  {
+    "revision": "4a0df3f28d5fc8754c2d",
+    "url": "/gesture-speech-recognition/static/js/runtime-main.a0c7b6c1.js"
+  }
+]);
